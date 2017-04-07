1354a524666e69766e400cfca40ca2cac9234b62
Taylo SpPank <tknapps31@gmail.com>
2017-04-07 12:54:43 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,4 @@
-function disableScroll() {
-  if (window.addEventListener) // older FF
-      window.addEventListener('DOMMouseScroll', preventDefault, false);
-  window.onwheel = preventDefault; // modern standard
-  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
-  window.ontouchmove  = preventDefault; // mobile
-  document.onkeydown  = preventDefaultForScrollKeys;
-}
+
 
 var touching = false;
 
