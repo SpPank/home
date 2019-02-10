6bf6b34ec4f368fb3fb184218e7f970de95ab4b6
Taylor Knapps <tknapps31@gmail.com>
2019-02-09 18:25:34 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -137,7 +137,7 @@ function setup() {
   imageMode(CENTER);
 
   target = "Taylor J Knapps";
-  popmax = 70;
+  popmax = 100;
   mutationRate = 0.1;
   population = new Population(target, mutationRate, popmax);
 }
